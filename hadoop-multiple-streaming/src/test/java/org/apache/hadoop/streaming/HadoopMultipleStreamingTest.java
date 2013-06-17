60a0d8a5b145c236ed298d0959e425686380dacb
hyonaldo <gyrbsdl18@naver.com>
2013-06-17 15:37:35 +0900

    test commit

---
+++ hadoop-multiple-streaming/src/test/java/org/apache/hadoop/streaming/HadoopMultipleStreamingTest.java
@@ -0,0 +1,14 @@
+package org.apache.hadoop.streaming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HadoopMultipleStreamingTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
